7ac41c598358b02faa0027a280d45b0481da8d64
hlyford <hlyford11@gmail.com>
2016-03-18 09:32:22 -0700

    solution for meeting times

--- meetingTimes.js
+++ meetingTimes.js
@@ -37,3 +37,54 @@
 // you plan to refactor HiCal to store times as Unix timestamps 
 // (which are big numbers). Write something that's efficient even when we 
 //can't put a nice upper bound on the numbers representing our time ranges.
+
+
+
+var condenseMeetingTimes = function(arr) {
+  var results = [];
+  // sort the meeting times by start time
+  var sortByStart = function(a, b, prop) {
+    prop = 'startTime';
+    return a[prop] > b[prop];
+  }
+  var arrSorted = arr.sort(sortByStart);    
+
+  // loop through and combine meetings if they overlap
+  var current = {startTime: arrSorted[0].startTime, endTime: arrSorted[0].endTime};
+  for (var i = 1; i < arrSorted.length; i++) {
+    // if they DONT overlap
+    if (!(current.endTime >= arrSorted[i].startTime)) {
+      // console.log(arrSorted[i].endTime);
+      results.push(current);
+      current = {startTime: arrSorted[i].startTime, endTime: arrSorted[i].endTime};
+    } else {
+      // DO overlap, continue the current meeting      
+      if (arrSorted[i].endTime > current.endTime) {        
+        // if it goes later, make new end point for current meeting        
+        current.endTime = arrSorted[i].endTime;
+      }
+    }
+  }
+  results.push(current);
+  return results;
+}
+
+// TEST: normal
+var times = [
+    {startTime: 0,  endTime: 1},
+    {startTime: 3,  endTime: 5},
+    {startTime: 4,  endTime: 8},
+    {startTime: 10, endTime: 12},
+    {startTime: 9,  endTime: 10},
+]
+
+// console.log(condenseMeetingTimes(times));
+
+// TEST: edge 1
+var times2 =   [
+    {startTime: 1, endTime: 10},
+    {startTime: 2, endTime: 6},
+    {startTime: 3, endTime: 5},
+    {startTime: 7, endTime: 9},
+]
+console.log(condenseMeetingTimes(times2));
